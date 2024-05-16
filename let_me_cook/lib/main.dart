ce12f11c1af71981d2f6d498dbcfaa18f0d27f70
Nhật Huy <89715333+nhathuy157@users.noreply.github.com>
2024-05-16 21:56:08 +0700

    Create SplashScreen and Intro
    Create

--- let_me_cook/lib/main.dart
+++ let_me_cook/lib/main.dart
@@ -1,115 +1,45 @@
-<<<<<<< Updated upstream
-import 'package:flutter/material.dart';
-=======
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:let_me_cook/constants/app_colors.dart';
-
 import 'package:let_me_cook/helpers/local_storage_helpers.dart';
 import 'package:let_me_cook/views/intro/splash_screen.dart';
 
-Future<void> main() async {
+void main() async {
+  // await Hive.initFlutter();
   await Hive.initFlutter();
   await LocalSrorageHelper.initLocalStorageHelper();
->>>>>>> Stashed changes
-
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  // This widget is the root of your application.
+  @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Let Me Cook',
+      title: 'Let me cook',
       theme: ThemeData(
-<<<<<<< Updated upstream
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-              style: TextStyles.defaultStyle,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-=======
         primaryColor: ColorPalette.primaryColor,
         scaffoldBackgroundColor: ColorPalette.bgScaffoldColor,
-        //backgroundColor: ColorPalette.bgScaffoldColor,
       ),
+      // routes: routes,
+      //onGenerateRoute: generateRoutes,
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
->>>>>>> Stashed changes
     );
   }
 }
